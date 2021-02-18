4ec12c348e2df74880cac5113348be2f578bddb5
lance <lancee478@gmail.com>
2021-02-18 14:41:19 -0700

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_socketio import SocketIO
+
+
+hostname = 'chapchats'
+socketio = SocketIO()
+
+
+def create_app(debug=False):
+    """Create an application."""
+    app = Flask(__name__)
+    app.debug = debug
+    app.config['SECRET_KEY'] = 'wtregfdvijlkj'
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    socketio.init_app(app)
+    return app
+
